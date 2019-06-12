bf77f99b33959ba3d13f7b1eeb042b997eb892db
pritamkhose <pritamkhose@gmaio.com>
2019-06-12 23:32:03 +0530

    Add codelab cupertino-store

---
+++ lib/codelab/cupertino-store-app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'cupertino-store-home.dart';
+
+void CupertinoStoreApp(){
+  runApp(
+      new MaterialApp(
+          home: new CupertinoStoreHome()
+      )
+  );
+}
