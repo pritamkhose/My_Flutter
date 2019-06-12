f6ef81d09f99598899520a727aca6aa9ec2e3c20
Pritam Khose <pritamkhose@gmail.com>
2019-06-13 00:28:23 +0530

    Update codelab cupertino-store

--- lib/codelab/cupertino-store-app.dart
+++ lib/codelab/cupertino-store-app.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
 
+import 'model/app_state_model.dart';
 import 'cupertino-store-home.dart';
 
+
 void CupertinoStoreApp(){
-  runApp(
-      new MaterialApp(
-          home: new CupertinoStoreHome()
-      )
+  // This app is designed only to work vertically, so we limit
+  // orientations to portrait up and down.
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  return runApp(
+    ChangeNotifierProvider<AppStateModel>(
+      builder: (context) => AppStateModel()..loadProducts(),
+      child: CupertinoStoreHome(),
+    ),
   );
+//  runApp(
+//      new MaterialApp(
+//          home: new CupertinoStoreHome()
+//      )
+//  );
 }
