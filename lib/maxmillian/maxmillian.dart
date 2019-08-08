79d588f47fe60b605b47f2f662cf4238b931a55d
Pritam Khose <pritamkhose@gmail.com>
2019-08-09 01:55:39 +0530

    Add MaxMillian tutorial

---
+++ lib/maxmillian/maxmillian.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:flutter/rendering.dart';
+
+import 'pages/auth.dart';
+import 'pages/products_admin.dart';
+import 'pages/products.dart';
+import 'pages/product.dart';
+import 'scoped-models/main.dart';
+import 'models/product.dart';
+import './shared/global_config.dart';
+
+// https://console.cloud.google.com/apis/credentials?folder=&organizationId=&project=securance-94e24
+// https://console.firebase.google.com/project/fiberbase-4b621/settings/general/android:com.example.my_flutter
+// https://console.firebase.google.com/u/1/project/fiberbase-4b621/database/fiberbase-4b621/data
+void main() => runApp(MaxmillionAPP());
+
+/*void main() => runApp(
+  // debugPaintSizeEnabled = true;
+  // debugPaintBaselinesEnabled = true;
+  // debugPaintPointersEnabled = true;
+  // MapView.setApiKey(apiKey);
+    MyApp()
+);*/
+
+class MaxmillionAPP extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Maxmillion APP',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyApp(),
+    );
+  }
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final MainModel _model = MainModel();
+  bool _isAuthenticated = false;
+
+  @override
+  void initState() {
+    _model.autoAuthenticate();
+    _model.userSubject.listen((bool isAuthenticated) {
+      setState(() {
+        _isAuthenticated = isAuthenticated;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('building main page');
+    return ScopedModel<MainModel>(
+      model: _model,
+      child: MaterialApp(
+        // debugShowMaterialGrid: true,
+        theme: ThemeData(
+            brightness: Brightness.light,
+            primarySwatch: Colors.deepOrange,
+            accentColor: Colors.deepPurple,
+            buttonColor: Colors.deepPurple),
+        //home: AuthPage(),
+        routes: {
+          '/': (BuildContext context) =>
+              !_isAuthenticated ? AuthPage() : ProductsPage(_model),
+          '/admin': (BuildContext context) =>
+              !_isAuthenticated ? AuthPage() : ProductsAdminPage(_model),
+        },
+        onGenerateRoute: (RouteSettings settings) {
+          if (!_isAuthenticated) {
+            return MaterialPageRoute<bool>(
+              builder: (BuildContext context) => AuthPage(),
+            );
+          }
+          final List<String> pathElements = settings.name.split('/');
+          if (pathElements[0] != '') {
+            return null;
+          }
+          if (pathElements[1] == 'product') {
+            final String productId = pathElements[2];
+            final Product product =
+                _model.allProducts.firstWhere((Product product) {
+              return product.id == productId;
+            });
+            return MaterialPageRoute<bool>(
+              builder: (BuildContext context) =>
+                  !_isAuthenticated ? AuthPage() : ProductPage(product),
+            );
+          }
+          return null;
+        },
+        onUnknownRoute: (RouteSettings settings) {
+          return MaterialPageRoute(
+              builder: (BuildContext context) =>
+                  !_isAuthenticated ? AuthPage() : ProductsPage(_model));
+        },
+      ),
+    );
+  }
+}
